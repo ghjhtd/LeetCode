8cddfbbd17cc6d5ed216115e0c926a804ec7c5b3
gonghaojing <1344081134@qq.com>
2022-03-08 21:51:20 +0800

    solve 5

---
+++ src/leetcode/editor/cn/ShortestPalindrome.java
@@ -0,0 +1,45 @@
+package leetcode.editor.cn;
+
+//给定一个字符串 s，你可以通过在字符串前面添加字符将其转换为回文串。找到并返回可以用这种方式转换的最短回文串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "aacecaaa"
+//输出："aaacecaaa"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "abcd"
+//输出："dcbabcd"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 仅由小写英文字母组成 
+// 
+// Related Topics 字符串 字符串匹配 哈希函数 滚动哈希 👍 423 👎 0
+
+public class ShortestPalindrome{
+    public static void main(String[] args) {
+        Solution solution = new ShortestPalindrome().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String shortestPalindrome(String s) {
+        return "";
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
