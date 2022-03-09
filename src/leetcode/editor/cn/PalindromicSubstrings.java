d6013bbf3018994d0b1a9801b068e525dc6f3c18
gonghaojing <1344081134@qq.com>
2022-03-09 23:20:21 +0800

    solve 647

---
+++ src/leetcode/editor/cn/PalindromicSubstrings.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+//给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。 
+//
+// 回文字符串 是正着读和倒过来读一样的字符串。 
+//
+// 子字符串 是字符串中的由连续字符组成的一个序列。 
+//
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "abc"
+//输出：3
+//解释：三个回文子串: "a", "b", "c"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "aaa"
+//输出：6
+//解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa" 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由小写英文字母组成 
+// 
+// Related Topics 字符串 动态规划 👍 795 👎 0
+
+public class PalindromicSubstrings{
+    public static void main(String[] args) {
+        Solution solution = new PalindromicSubstrings().new Solution();
+        System.out.println(solution.countSubstrings("abc"));
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int a = 0, b = 0;
+        for(int i = 0; i < s.length(); i++){
+            a = count_len(s, i, i)/2 + 1;
+            b = count_len(s, i, i+1)/2;
+            count += a;
+            count += b;
+        }
+        return count;
+    }
+
+    public int count_len(String s, int left, int right){
+        int length = 0;
+        int temp = right - left;
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+            length ++;
+        }
+        return (length-1)*2 + temp + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
