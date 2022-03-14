a3b3dcbefc1d246afce079f39fead53a7d2a682f
gonghaojing <1344081134@qq.com>
2022-03-14 10:38:19 +0800

    solve 190

--- src/leetcode/editor/cn/UglyNumber.java
+++ src/leetcode/editor/cn/UglyNumber.java
@@ -55,7 +55,16 @@ public class UglyNumber{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isUgly(int n) {
-
+        if(n <= 0){
+            return false;
+        }
+        int[] nums = {2,3,5};
+        for(int num : nums){
+            while (n % num == 0){
+                n /= num;
+            }
+        }
+        return n==1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
