ad2284832ec92ede3d5d695983e1669b3ef87ae8
gonghaojing <1344081134@qq.com>
2022-03-26 18:32:40 +0800

    solve 445

---
+++ src/leetcode/editor/cn/AddTwoNumbersIi.java
@@ -0,0 +1,106 @@
+package leetcode.editor.cn;
+
+//给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。 
+//
+// 你可以假设除了数字 0 之外，这两个数字都不会以零开头。 
+//
+// 
+//
+// 示例1： 
+//
+// 
+//
+// 
+//输入：l1 = [7,2,4,3], l2 = [5,6,4]
+//输出：[7,8,0,7]
+// 
+//
+// 示例2： 
+//
+// 
+//输入：l1 = [2,4,3], l2 = [5,6,4]
+//输出：[8,0,7]
+// 
+//
+// 示例3： 
+//
+// 
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表的长度范围为 [1, 100] 
+// 0 <= node.val <= 9 
+// 输入数据保证链表代表的数字无前导 0 
+// 
+//
+// 
+//
+// 进阶：如果输入链表不能翻转该如何解决？ 
+// Related Topics 栈 链表 数学 👍 495 👎 0
+
+import java.util.Stack;
+
+public class AddTwoNumbersIi{
+    public static void main(String[] args) {
+        Solution solution = new AddTwoNumbersIi().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null){
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null){
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        ListNode list = new ListNode();
+        int temp = 0;
+        int a = 0;
+        int b = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || temp != 0){
+            a = stack1.isEmpty() ? 0 : stack1.pop();
+            b = stack2.isEmpty() ? 0 : stack2.pop();
+            list = add_tail(list, (a + b + temp)%10);
+            temp = ((a + b + temp)/10);
+        }
+        return list.next;
+    }
+
+    public ListNode add_tail(ListNode head, int val){
+        ListNode pre = new ListNode(val);
+        pre.next = head.next;
+        head.next = pre;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+}
