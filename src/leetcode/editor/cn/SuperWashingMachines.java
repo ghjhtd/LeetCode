3e89aea36e1060d1c72f6e8e385bd331661150c3
gonghaojing <1344081134@qq.com>
2022-04-23 23:01:40 +0800

    solve 134

--- src/leetcode/editor/cn/SuperWashingMachines.java
+++ src/leetcode/editor/cn/SuperWashingMachines.java
@@ -53,13 +53,28 @@ package leetcode.editor.cn;
 public class SuperWashingMachines{
     public static void main(String[] args) {
         Solution solution = new SuperWashingMachines().new Solution();
-        
+        System.out.println(solution.findMinMoves(new int[]{0, 0 ,11, 5}));
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findMinMoves(int[] machines) {
-
+        int total = 0;
+        for(int i : machines){
+            total += i;
+        }
+        if(total % machines.length != 0){
+            return -1;
+        }
+        int sum = 0;
+        int ans = 0;
+        int average = total/machines.length;
+        for(int i : machines){
+            i -= average;
+            sum += i;
+            ans = Math.max(ans, Math.max(i, Math.abs(sum)));
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
