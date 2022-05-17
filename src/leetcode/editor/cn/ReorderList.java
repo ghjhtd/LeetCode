97709b9e90ac63d7a798f3e209b6c754c5dc752c
gonghaojing <1344081134@qq.com>
2022-05-17 23:37:37 +0800

    solve 437

---
+++ src/leetcode/editor/cn/ReorderList.java
@@ -0,0 +1,95 @@
+package leetcode.editor.cn;
+
+//给定一个单链表 L 的头节点 head ，单链表 L 表示为： 
+//
+// 
+//L0 → L1 → … → Ln - 1 → Ln
+// 
+//
+// 请将其重新排列后变为： 
+//
+// 
+//L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → … 
+//
+// 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[1,4,2,3] 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[1,5,2,4,3] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表的长度范围为 [1, 5 * 10⁴] 
+// 1 <= node.val <= 1000 
+// 
+// Related Topics 栈 递归 链表 双指针 👍 907 👎 0
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ReorderList{
+    public static void main(String[] args) {
+        Solution solution = new ReorderList().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        Deque<ListNode> stack = new LinkedList<>();
+        ListNode list = new ListNode();
+        list.next = head;
+        while (head != null){
+            stack.addLast(head);
+            head = head.next;
+        }
+        int index = 1;
+        ListNode temp = list;
+        while (!stack.isEmpty()){
+            if(index++ % 2 == 1){
+                temp.next = stack.pollFirst();
+            }else{
+                temp.next = stack.pollLast();
+            }
+            temp = temp.next;
+            temp.next = null;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+}
